3633aa905a7b679c636e11ede3346be9128eeb74
Christian Lambino <chdi.lambino.up@phinmaed.com>
2023-12-09 20:35:28 +0800

    Quiz 1

--- app/src/main/java/com/lambino/christian/block6/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/lambino/christian/block6/p1/quiz/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello Welcome to the Android $name!",
+        text = "Hello Welcome $name!",
         modifier = modifier
     )
 }
